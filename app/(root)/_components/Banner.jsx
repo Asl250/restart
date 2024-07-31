bdff4d86b535fac65eb9a56e0680179f51996dec
ask250 <aslrahidov250@gmail.com>
2024-07-31 11:37:22 +0500

    5 commit

--- app/(root)/_components/Banner.jsx
+++ app/(root)/_components/Banner.jsx
@@ -7,14 +7,13 @@ import star from '/public/star.svg'
 
 const Banner = () => {
 	return (
-		<div className={'bg-[#141522] w-full py-24'}>
+		<div className={'bg-[#141522] w-full py-4'}>
 			
 			
 			<div>
-				<div className={'w-full text-center text-white '}>
-					<h1 className={'text-5xl font-semibold text-center leading-normal'}>UY SOTIB OLISHDA <br/> ALDANIB QOLMANG!</h1>
-					<p className={' text-center text-lg max-xl:justify-center'}>2024-yilda uy sotib olishdan oldin nimalarga <br/> e’tibor berishingiz kerak? Quyidagi videoni ko’ring va batafsil bilib oling.</p>
-
+				<div className={'w-full text-center text-white mb-[17rem]'}>
+					<h1 className={'text-5xl oswald font-[74px] text-center leading-normal pb-5'}>UY SOTIB OLISHDA <br/> ALDANIB QOLMANG!</h1>
+					<p className={' text-center text-md max-xl:justify-center px-5'}>2024-yilda uy sotib olishdan oldin nimalarga e’tibor berishingiz kerak? <br className={'md:block hidden'}/> Quyidagi videoni ko’ring va batafsil bilib oling.</p>
 				</div>
 				
 				{/*<div className={'xl:z-0 xl:pl-[55%] xl:-mt-[140px] max-lg:mx-[5%] lg:mx-[10%] xl:mx-0 max-sm:mx-0'}>*/}
