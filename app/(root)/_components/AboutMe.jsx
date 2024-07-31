0f2e015dc734287af602ccd8b0c806cfaf680fe7
ask250 <aslrahidov250@gmail.com>
2024-07-31 12:22:54 +0500

    7 commit

--- app/(root)/_components/AboutMe.jsx
+++ app/(root)/_components/AboutMe.jsx
@@ -7,30 +7,54 @@ const AboutMe = () => {
 			<div className={' mb-5'}>
 				<Image className={'w-[50%] max-lg:w-[90%] mx-auto aspect-square rounded-full'} src={'/about1.JPG'} alt={'about1'} width={450} height={450} />
 			</div>
-			<div className={'inter font-[18px] w-full text-center'}>
+			<div className={'inter font-[18px]  text-start'}>
 				<div className={"px-[200px] max-lg:px-[50px]"}>
-					<h1 className={'text-3xl font-semibold mb-5'}>Assalomu Alaykum, Ismim Behzod Umarov.</h1>
-					<p className={'text-xl leading-[2]'}>Ko’chmas mulk savdosi bo’yicha 4 yillik tajribaga egaman.
+					<p className={'text-xl leading-[2] max-sm:text-sm'}>Assalomu Alaykum, Ismim Behzod Umarov.
+						<br/>
+						Ko’chmas mulk savdosi bo’yicha 4 yillik tajribaga egaman.
+						<br/>
 						Ushbu vaqt davomida 100 dan ortiq uy sotib olmoqchi bo’lganlar bilan gaplashgan bo’lsam…
+						<br/>
 						Ko’pchilikda deyarli bir xil muammoni kuzatardim.
+						<br/>
 						10-15 yil davomida yig’ib-terib endi uy olaman deganda aldanib qolishlik…
+						<br/>
 						Ishonchsiz quruvchini tanlab qo’yishlik…
+						<br/>
 						To’g’ri shartnoma qilmaslik…
+						<br/>
 						Uylarni vaqtida bitmasligi…
+						
+						<br/>
 						Quruvchini katta va’dalariga ishonib qolish va hokazo.
+						<br/>
 						Sanaydigan bo’lsam, muammolar juda ham ko’p.
+						<br/>
 						Aksariyat odamlarni bu muammolarga uchrashini sababi…
+						<br/>
 						Bu - ularni ko’chmas mulk bo’yicha yetarlicha bilimga ega emasliklaridir.
+						<br/>
 						Bilimlari yo’qligi uchun esa…
+						<br/>
 						“Endi mani ham uyim bo’ladi” deb yaxshi niyatlarda yeg’gan
+						<br/>
 						$60,000…
+						<br/>
 						$80,000…
+						<br/>
 						Kerak bo’lsa $100,000 lariga ham aldanib qolishadi.
-						Bunday holatlarni juda ko’p kuzatdim. Bilasizmi, ko’pchilik uy sotib olishdan oldin asosan
-						nimaga ko’proq e’tibor berishadi?
+						<br/>
+						Bunday holatlarni juda ko’p kuzatdim.
+						<br/>
+						Bilasizmi, ko’pchilik uy sotib olishdan oldin asosan nimaga ko’proq e’tibor berishadi?
+						<br/>
 						Narxiga!
+						<br/>
 						Naxi qulay bo’lsa tezda dogovor qilib yuborishadi!
+						<br/>
+						
 						Uyni tarixiga…
+					
 					</p>
 				</div>
 				
@@ -38,28 +62,29 @@ const AboutMe = () => {
 			
 			
 			<div className={'mt-24'}>
-				<div className={'w-full text-center'}>
+				<div className={'text-start'}>
 					
 					<div className={'px-[200px] max-lg:px-[50px]'}>
-						<h1 className={'text-3xl font-semibold mb-5'}>Kerakli xujjatlariga…</h1>
-						<p className={'text-xl  leading-[2.5]'}>Shartnoma ko’proq quruvchini foydasigami yoki mijozni foydasiga tuzilganmi?
-							
+						<p className={'text-xl max-sm:text-sm  leading-[2.5]'}>Kerakli xujjatlariga…
+							<br/>
+							Shartnoma ko’proq quruvchini foydasigami yoki mijozni foydasiga tuzilganmi?
+							<br/>
 							Bu narsalarga e’tibor berishmaydi.
-							
+							<br/>
 							Quruvchini ishonchliligi bilan xujjatlari joyida bo’lmasa, uyingizni qurilishi ancha cho’zilib ketishi mumkin
-							
+							<br/>
 							Yoki quruvchi pullaringizni o’zlashtirib, ertaga “bankrot bo’ldik” deb e’lon qilib yuborishi ham hech gap emas.
-							
+							<br/>
 							Undan tashqari, agar hujjatlari joyida bo’lmasa bino qurilib bo’lganidan keyin kadastri chiqmasligi mumkin.
-							
+							<br/>
 							Qurilish sifatsiz bo’lib qolishi mumkin…
-							
+							<br/>
 							Bularning barchasi uy sotib olish bo’yicha yetarlicha bilimga ega bo’lmaganlik sabab yuz beradi.
-							
+							<br/>
 							Agar siz ham yaqin orada uy olishni reja qilib yurgan bo’lsangiz…
-							
+							<br/>
 							Xavotirga o’rin yo’q.
-							
+							<br/>
 							Biz jamoamiz bilan sizga ko’chmas mulk bo’yicha bilimingizni oshirishga yordam beradigan  video darsliklar ishlab chiqdik.
 						</p>
 					</div>
