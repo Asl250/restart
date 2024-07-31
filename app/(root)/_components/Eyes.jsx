5ea8e7eca6f61df0b9cd76333f52640b2b4ec4e0
ask250 <aslrahidov250@gmail.com>
2024-07-31 16:46:12 +0500

    final commit

--- app/(root)/_components/Eyes.jsx
+++ app/(root)/_components/Eyes.jsx
@@ -8,9 +8,9 @@ const Eyes = () => {
 					alt={'token'} width={500} height={300} className={'w-[65%] mb-10 max-lg:mx-auto'} />
 			</div>
 			
-			<h1 className={'w-full inter text-7xl max-md:text-5xl text-center font-semibold text-green-400'}>“Ko’zlarimga
+			<h1 className={'w-full inter text-7xl max-md:text-5xl text-center text-green-400'}>“Ko’zlarimga
 				Ishonmayman”</h1>
-			<h1 className={'w-full inter text-2xl text-center font-semibold text-white mt-4'}>nomli to’lovingizni 100%
+			<h1 className={'w-full inter text-2xl text-center text-white mt-4'}>nomli to’lovingizni 100%
 				to’liq<br className={'hidden max-md:block'} /> holda qaytarib berish kafolati</h1>
 			<div className={'mt-14 xl:mx-[7%] max-lg:mx-[5%] text-start grid lg:grid-cols-2'}>
 				<div className={'max-lg:w-full max-lg:mx-[10%] max-sm:mx-0 hidden lg:flex'}>
@@ -18,8 +18,8 @@ const Eyes = () => {
 						src={'https://optim.tildacdn.one/tild6437-3032-4530-a534-323133313262/-/resize/870x/-/format/webp/guarantee-1.png'}
 						alt={'token'} width={500} height={300} />
 				</div>
-				<div className={'inter space-y-5 py-5'}>
-					<p className={'text-2xl w-full text-white'}>Ushbu darslarni yozish uchun ko’plab o’z ishini
+				<div className={'inter space-y-5 py-5 max-lg:ml-[7%]'}>
+					<p className={'text-2xl inter font-[18px] w-full text-white'}>Ushbu darslarni yozish uchun ko’plab o’z ishini
 						mutaxassilari mehnat qildi</p>
 					<p className={'text-2xl w-full text-white'}>Va bu darslar sizga yoqishiga men kafolat beraman.</p>
 					<p className={'text-2xl w-full text-white'}>Agarda darslarni olib, ko’rib chiqsangiz-u…</p>
