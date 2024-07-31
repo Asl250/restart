04b3719f6460d3c12c33e1c6418c5787019085b6
ask250 <aslrahidov250@gmail.com>
2024-07-31 12:52:42 +0500

    12 commit

--- app/(root)/_components/Opportunity.jsx
+++ app/(root)/_components/Opportunity.jsx
@@ -24,11 +24,11 @@ const Opportunity = () => {
 					
 					<div className={'space-y-3'}>
 						<p className={'mt-3 font-semibold text-2xl mb-5'}> Qanaqa qilib uylarni to’g’ri sotib olishni o’rganasiz</p>
-						<p className={'font-semibold max-w-xs text-xl pt-4'}>- Qaysi etajdan uy tanlash kerak?</p>
-						<p className={'font-semibold max-w-xs text-xl pt-4'}>- Qaysi lokatsiyadan uy olish kerak?</p>
-						<p className={'font-semibold max-w-xs text-xl pt-4'}>- Quruvchini ishonchliligini qanaqa qilib tekshirsa bo’ladi? </p>
-						<p className={'font-semibold max-w-xs text-xl pt-4'}>- Qanaqa qilib quruvchi haqida to’liq ma’lumot olsa bo’ladi? </p>
-						<p className={'font-semibold max-w-xs text-xl pt-4'}>Shuni hammasini bilib olasiz.</p>
+						<p className={'max-w-xs text-xl pt-4'}>- Qaysi etajdan uy tanlash kerak?</p>
+						<p className={' max-w-xs text-xl pt-4'}>- Qaysi lokatsiyadan uy olish kerak?</p>
+						<p className={' max-w-xs text-xl pt-4'}>- Quruvchini ishonchliligini qanaqa qilib tekshirsa bo’ladi? </p>
+						<p className={' max-w-xs text-xl pt-4'}>- Qanaqa qilib quruvchi haqida to’liq ma’lumot olsa bo’ladi? </p>
+						<p className={' max-w-xs text-xl pt-4'}>Shuni hammasini bilib olasiz.</p>
 					</div>
 				
 				
@@ -42,12 +42,12 @@ const Opportunity = () => {
 						height={50}
 					/>
 					
-					<div className={'space-y-3'}>
+					<div className={'space-y-7'}>
 						<p className={'mt-3 font-semibold text-2xl mb-5'}>Xujjatlar bilan ishlashni o’rganasiz</p>
-						<p className={'font-semibold max-w-xs text-lg'}>- Quruvchini proyektga oid qanaqa xujjatlari bor yoki qanaqalari yo’q?</p>
-						<p className={'font-semibold max-w-xs text-lg'}>Hammasini to’liq ko’rib chiqa olasiz va to’liq tushuna olasiz. </p>
-						<p className={'font-semibold max-w-xs text-lg'}>Xujjatlarida qandaydir kamchiliklar bo’lsa, darhol sezasiz.</p>
-						<p className={'font-semibold max-w-xs text-lg'}>Undan tashqari, qurilishni qaysi etapi to’g’ri qurilyapti, qayeri noto’g’ri ketyapti, hammasi reja bo’yicha ketyaptimi yoki rejaga o’zgartirish kiritildimi hammasini bila olasiz.</p>
+						<p className={' max-w-xs text-lg'}>- Quruvchini proyektga oid qanaqa xujjatlari bor yoki qanaqalari yo’q?</p>
+						<p className={' max-w-xs text-lg'}>Hammasini to’liq ko’rib chiqa olasiz va to’liq tushuna olasiz. </p>
+						<p className={' max-w-xs text-lg'}>Xujjatlarida qandaydir kamchiliklar bo’lsa, darhol sezasiz.</p>
+						<p className={' max-w-xs text-lg'}>Undan tashqari, qurilishni qaysi etapi to’g’ri qurilyapti, qayeri noto’g’ri ketyapti, hammasi reja bo’yicha ketyaptimi yoki rejaga o’zgartirish kiritildimi hammasini bila olasiz.</p>
 					</div>
 				
 				
@@ -61,12 +61,12 @@ const Opportunity = () => {
 						height={50}
 					/>
 					
-					<div className={'space-y-3'}>
+					<div className={'space-y-7'}>
 						<p className={'mt-3 font-semibold text-2xl mb-5'}>Shartnoma bilan ishlashni o’rganasiz.</p>
-						<p className={'font-semibold max-w-xs text-lg'}>- Shartnomada qaysi bandlar bo’lishi kerak? </p>
-						<p className={'font-semibold max-w-xs text-lg'}>- Shartoma tuzayotganda nimalarga e’tibor berish kerak? </p>
-						<p className={'font-semibold max-w-xs text-lg'}>- Quruvchi bilan ertaga tushunmovchiliklar kelib chiqadigan bo’lsa, siz yutib chiqishingiz uchun shartnomaga qanday bandlarni qo’shtirib olishingiz kerak? </p>
-						<p className={'font-semibold max-w-xs text-lg'}>Shu bo’yicha to’liq ma’lumotlarga ega bo’lasiz.</p>
+						<p className={' max-w-xs text-lg'}>- Shartnomada qaysi bandlar bo’lishi kerak? </p>
+						<p className={' max-w-xs text-lg'}>- Shartoma tuzayotganda nimalarga e’tibor berish kerak? </p>
+						<p className={'max-w-xs text-lg'}>- Quruvchi bilan ertaga tushunmovchiliklar kelib chiqadigan bo’lsa, siz yutib chiqishingiz uchun shartnomaga qanday bandlarni qo’shtirib olishingiz kerak? </p>
+						<p className={' max-w-xs text-lg'}>Shu bo’yicha to’liq ma’lumotlarga ega bo’lasiz.</p>
 					</div>
 				
 				
