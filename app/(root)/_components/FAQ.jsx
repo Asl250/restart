5849ef691418e9930e5d1c9591623ce424306b26
ask250 <aslrahidov250@gmail.com>
2024-07-31 22:19:16 +0500

    FAQ

--- app/(root)/_components/FAQ.jsx
+++ app/(root)/_components/FAQ.jsx
@@ -11,8 +11,8 @@ const FAQ = ( ) => {
 			<h1 className={'w-full text-4xl text-center font-semibold mb-24'}> Eng Ko’p Beriladigan Savollar </h1>
 			<Accordion type="single" collapsible className="w-full">
 				<AccordionItem value="item-1" className={'mb-5 border-[2px] rounded-lg p-3'}>
-					<AccordionTrigger className={'text-xl text-start max-md:text-lg max-sm:text-lg'}>497,000 so’mdan necha marta to’laymiz?</AccordionTrigger>
-					<AccordionContent className={'text-xl max-md:text-lg'}>
+					<AccordionTrigger className={'text-lg text-start max-md:text-lg max-sm:text-lg'}>497,000 so’mdan necha marta to’laymiz?</AccordionTrigger>
+					<AccordionContent className={'text-lg'}>
 						Bir marta 497,000 so’m to’laysiz va video darsliklarga 6 oylik dostupga ega bo’lasiz.
 						<br/>
 						<br/>
@@ -20,8 +20,8 @@ const FAQ = ( ) => {
 					</AccordionContent>
 				</AccordionItem>
 				<AccordionItem value="item-2" className={'mb-5  border-[2px] rounded-lg p-3'}>
-					<AccordionTrigger className={'text-xl text-start max-md:text-lg max-sm:text-lg'}>Yangi darsliklar ham qo’shiladimi? </AccordionTrigger>
-					<AccordionContent className={'text-xl max-md:text-lg'}>
+					<AccordionTrigger className={'text-lg text-start max-md:text-lg max-sm:text-lg'}>Yangi darsliklar ham qo’shiladimi? </AccordionTrigger>
+					<AccordionContent className={'text-lg '}>
 						Ha! Har oyda bir marta qurilish oid kuchli spikerlarni chaqiramiz, ma’lumotlar olamiz
 						<br />
 						<br />
@@ -29,14 +29,14 @@ const FAQ = ( ) => {
 					</AccordionContent>
 				</AccordionItem>
 				<AccordionItem value="item-3" className={'mb-5 border-[2px] rounded-lg p-3 text-start'}>
-					<AccordionTrigger className={'text-xl max-md:text-lg max-sm:text-lg text-start'}>Barcha darslar darhol ochiladimi? </AccordionTrigger>
-					<AccordionContent className={'text-xl max-md:text-lg'}>
+					<AccordionTrigger className={'text-lg max-md:text-lg max-sm:text-lg text-start'}>Barcha darslar darhol ochiladimi? </AccordionTrigger>
+					<AccordionContent className={'text-lg max-md:text-lg'}>
 						Ha! Yopiq telegram kanalga qo’shilishingiz bilanoq barcha darslarni ko’rish imkoniyatiga ega bo’lasiz
 					</AccordionContent>
 				</AccordionItem>
 				<AccordionItem value="item-4" className={'mb-5 border-[2px] rounded-lg p-3'}>
-					<AccordionTrigger className={'text-xl max-md:text-lg max-sm:text-lg text-start'}>Bu darsliklardan qanday foyda olaman?</AccordionTrigger>
-					<AccordionContent className={'text-xl max-md:text-lg'}>
+					<AccordionTrigger className={'text-lg max-md:text-lg max-sm:text-lg text-start'}>Bu darsliklardan qanday foyda olaman?</AccordionTrigger>
+					<AccordionContent className={'text-lg'}>
 						Ushbu darsliklar orqali ham ko’chmas mulk bo’yicha savodxonligingiz oshadi ham yuridik tomonlama
 						maslahatlar beriladi.
 						<br />
