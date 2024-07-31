8a554a64abe658a20254ce1ff156ab7c176fe371
ask250 <aslrahidov250@gmail.com>
2024-07-31 18:33:03 +0500

    Oppurtunity

--- app/(root)/_components/Opportunity.jsx
+++ app/(root)/_components/Opportunity.jsx
@@ -13,17 +13,11 @@ const Opportunity = () => {
 			</div>
 			
 			<div className={'mx-[10%] mt-10 grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-10'}>
-				
-				<div className={'bg-[#313242] rounded-lg p-5'}>
-					<Image
-						src={house}
-						alt={'house'}
-						width={50}
-						height={50}
-					/>
-					
-					<div className={'space-y-3'}>
-						<p className={'mt-3 font-semibold text-2xl mb-5'}> Qanaqa qilib uylarni to’g’ri sotib olishni o’rganasiz</p>
+				<div className={'bg-black rounded-lg p-5'}>
+					<h1 className={'text-2xl'}>Modul 1</h1>
+					<hr className={'w-full p-px'}/>
+					<div className={'space-y-3 '}>
+						<p className={'mt-3 font-semibold text-2xl mb-5 text-[#feebbd]'}> Qanaqa qilib uylarni to’g’ri sotib olishni o’rganasiz</p>
 						<p className={'max-w-xs text-lg pt-4'}>- Qaysi etajdan uy tanlash kerak?</p>
 						<p className={' max-w-xs text-lg pt-4'}>- Qaysi lokatsiyadan uy olish kerak?</p>
 						<p className={' max-w-xs text-lg pt-4'}>- Quruvchini ishonchliligini qanaqa qilib tekshirsa bo’ladi? </p>
@@ -34,38 +28,37 @@ const Opportunity = () => {
 				
 				</div>
 				
-				<div className={'bg-[#313242] rounded-lg p-5'}>
-					<Image
-						src={success}
-						alt={'house'}
-						width={50}
-						height={50}
-					/>
-					
+				<div className={'bg-black rounded-lg p-5'}>
+					<h1 className={'text-2xl'}>Modul 2</h1>
+					<hr className={'w-full p-px'} />
 					<div className={'space-y-7'}>
-						<p className={'mt-3 font-semibold text-2xl mb-5'}>Xujjatlar bilan ishlashni o’rganasiz</p>
-						<p className={' max-w-xs text-lg'}>- Quruvchini proyektga oid qanaqa xujjatlari bor yoki qanaqalari yo’q?</p>
-						<p className={' max-w-xs text-lg'}>Hammasini to’liq ko’rib chiqa olasiz va to’liq tushuna olasiz. </p>
-						<p className={' max-w-xs text-lg'}>Xujjatlarida qandaydir kamchiliklar bo’lsa, darhol sezasiz.</p>
-						<p className={' max-w-xs text-lg'}>Undan tashqari, qurilishni qaysi etapi to’g’ri qurilyapti, qayeri noto’g’ri ketyapti, hammasi reja bo’yicha ketyaptimi yoki rejaga o’zgartirish kiritildimi hammasini bila olasiz.</p>
+						<p className={'mt-3 font-semibold text-2xl mb-5 text-[#feebbd]'}>Xujjatlar bilan ishlashni
+							o’rganasiz</p>
+						<p className={' max-w-xs text-lg'}>- Quruvchini proyektga oid qanaqa xujjatlari bor yoki
+							qanaqalari yo’q?</p>
+						<p className={' max-w-xs text-lg'}>Hammasini to’liq ko’rib chiqa olasiz va to’liq tushuna
+							olasiz. </p>
+						<p className={' max-w-xs text-lg'}>Xujjatlarida qandaydir kamchiliklar bo’lsa, darhol
+							sezasiz.</p>
+						<p className={' max-w-xs text-lg'}>Undan tashqari, qurilishni qaysi etapi to’g’ri qurilyapti,
+							qayeri noto’g’ri ketyapti, hammasi reja bo’yicha ketyaptimi yoki rejaga o’zgartirish
+							kiritildimi hammasini bila olasiz.</p>
 					</div>
 				
 				
 				</div>
 				
-				<div className={'bg-[#313242] rounded-lg p-5'}>
-					<Image
-						src={vint}
-						alt={'house'}
-						width={50}
-						height={50}
-					/>
-					
+				<div className={'bg-black rounded-lg p-5'}>
+					<h1 className={'text-2xl'}>Modul 3</h1>
+					<hr className={'w-full p-px'} />
 					<div className={'space-y-7'}>
-						<p className={'mt-3 font-semibold text-2xl mb-5'}>Shartnoma bilan ishlashni o’rganasiz.</p>
+						<p className={'mt-3 font-semibold text-2xl mb-5 text-[#feebbd]'}>Shartnoma bilan ishlashni
+							o’rganasiz.</p>
 						<p className={' max-w-xs text-lg'}>- Shartnomada qaysi bandlar bo’lishi kerak? </p>
 						<p className={' max-w-xs text-lg'}>- Shartoma tuzayotganda nimalarga e’tibor berish kerak? </p>
-						<p className={'max-w-xs text-lg'}>- Quruvchi bilan ertaga tushunmovchiliklar kelib chiqadigan bo’lsa, siz yutib chiqishingiz uchun shartnomaga qanday bandlarni qo’shtirib olishingiz kerak? </p>
+						<p className={'max-w-xs text-lg'}>- Quruvchi bilan ertaga tushunmovchiliklar kelib chiqadigan
+							bo’lsa, siz yutib chiqishingiz uchun shartnomaga qanday bandlarni qo’shtirib olishingiz
+							kerak? </p>
 						<p className={' max-w-xs text-lg'}>Shu bo’yicha to’liq ma’lumotlarga ega bo’lasiz.</p>
 					</div>
 				
