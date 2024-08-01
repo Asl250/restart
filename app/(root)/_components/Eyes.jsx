1c0e7fb56c4eb96b004e1d1cdd9e42bab59e3077
ask250 <aslrahidov250@gmail.com>
2024-08-01 16:24:54 +0500

    commit

--- app/(root)/_components/Eyes.jsx
+++ app/(root)/_components/Eyes.jsx
@@ -1,33 +1,31 @@
 import Image from 'next/image'
 const Eyes = () => {
 	return (
-		<div className={'bg-black  py-14'}>
-			<div className={'max-lg:w-full max-lg:mx-auto max-sm:mx-0 hidden max-lg:block'}>
-				<Image
-					src={'https://optim.tildacdn.one/tild6437-3032-4530-a534-323133313262/-/resize/870x/-/format/webp/guarantee-1.png'}
-					alt={'token'} width={500} height={300} className={'w-[65%] mb-10 max-lg:mx-auto'} />
+		<div className={'text-black py-24'}>
+			<div className={'flex justify-center'}>
+				<Image src={'/left.png'} alt={'left'} width={100} height={100}/>
+				<div
+					className={'pt-10 px-[-10px] mx-[-10px] font-[800] inter text-7xl max-md:text-5xl max-sm:text-2xl text-center'}>“Ko’zlarimga
+					Ishonmayman”
+					<h1 className={' inter max-md:text-sm text-lg text-center mt-4'}>nomli to’lovingizni 100%
+						to’liq holda qaytarib berish kafolati</h1>
+				</div>
+				<Image src={'/right.png'} alt={'left'} width={100} height={100}  />
+			
 			</div>
 			
-			<h1 className={'w-full font-semibold text-[#feebbd] inter text-7xl max-md:text-5xl text-center'}>“Ko’zlarimga
-				Ishonmayman”</h1>
-			<h1 className={'w-full inter text-lg text-center text-white mt-4'}>nomli to’lovingizni 100%
-				to’liq<br className={'hidden max-md:block'} /> holda qaytarib berish kafolati</h1>
-			<div className={'mt-14 xl:mx-[7%] max-lg:mx-[5%] text-start grid lg:grid-cols-2'}>
-				<div className={'max-lg:w-full max-lg:mx-[10%] max-sm:mx-0 hidden lg:flex'}>
-					<Image
-						src={'https://optim.tildacdn.one/tild6437-3032-4530-a534-323133313262/-/resize/870x/-/format/webp/guarantee-1.png'}
-						alt={'token'} width={500} height={300} />
-				</div>
-				<div className={'text-lg border border-[#feebbd] rounded-xl p-5 inter space-y-7 py-5'}>
-					<p className={'inter w-full text-white'}>Ushbu darslarni yozish uchun ko’plab o’z ishini
+			
+			<div className={'mt-14 text-center max-lg:text-start max-lg:px-[50px]'}>
+				<div className={'text-xl rounded-xl inter space-y-7 py-5'}>
+					<p className={'inter w-full '}>Ushbu darslarni yozish uchun ko’plab o’z ishini
 						mutaxassilari mehnat qildi</p>
-					<p className={'w-full text-white'}>Va bu darslar sizga yoqishiga men kafolat beraman.</p>
-					<p className={'w-full text-white'}>Agarda darslarni olib, ko’rib chiqsangiz-u…</p>
-					<p className={'w-full text-white'}>Va ushbu darslikdagi ma’lumotlar 497,000 so’mga
+					<p className={'w-full '}>Va bu darslar sizga yoqishiga men kafolat beraman.</p>
+					<p className={'w-full '}>Agarda darslarni olib, ko’rib chiqsangiz-u…</p>
+					<p className={'w-full '}>Va ushbu darslikdagi ma’lumotlar 497,000 so’mga
 						arzishiga ishonch hosil qilmasangiz,</p>
-					<p className={' w-full text-white'}>Bizga darsni xarid qilgan sanangizdan boshlab, 3 kun
+					<p className={' w-full '}>Bizga darsni xarid qilgan sanangizdan boshlab, 3 kun
 						ichida murojaat qiling. </p>
-					<p className={'w-full  text-white'}>To’lovingizni 100% to’liq holda qaytarib beramiz.</p>
+					<p className={'w-full  '}>To’lovingizni 100% to’liq holda qaytarib beramiz.</p>
 				</div>
 			
 			</div>
