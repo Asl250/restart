852c570577ba37854553d053dc7c3e7855dcea12
ask250 <aslrahidov250@gmail.com>
2024-08-01 13:02:08 +0500

    text

--- app/(root)/_components/AboutMe.jsx
+++ app/(root)/_components/AboutMe.jsx
@@ -9,23 +9,24 @@ const AboutMe = () => {
 			</div>
 			<div className={'inter font-[18px] text-start'}>
 				<div className={'px-[200px] max-lg:px-[50px]'}>
+							<p className={'text-lg text-start mb-16'}>Behzod Umarov ofisidan,
+								<br/>
+								Yunusobod, Toshkent.
+							</p>
 							<p className={'text-lg'}>
-								Assalomu Alaykum, Ismim Behzod Umarov.
-								<br/><br/>
-								Ko’chmas mulk savdosi bo’yicha 4 yillik tajribaga egaman.
-								<br/><br/>
-								Ushbu vaqt davomida 100 dan ortiq uy sotib olmoqchi bo’lganlar bilan gaplashgan bo’lsam…
+								Hurmatli yaqin orada uy olishni rejalashtirayotgan do’stim…
+								Oxirigi 4 yil ichida 100 dan ortiq uy sotib olmoqchi bo’lganlar bilan gaplashgan bo’lsam…
 								<br/><br/>
 								Ko’pchilikda deyarli bir xil muammoni kuzatardim.
 								<br/><br/>
-								10-15 yil davomida yig’ib-terib endi uy olaman deganda aldanib qolishlik…
+								<b>10-15 yil davomida yig’ib-terib endi uy olaman deganda aldanib qolishlik…</b>
 								<br/><br/>
 								Ishonchsiz quruvchini tanlab qo’yishlik…
 								<br/><br/>
 								To’g’ri shartnoma qilmaslik…
+								
 								<br/><br/>
 								Uylarni vaqtida bitmasligi…
-								
 								<br/><br/>
 								Quruvchini katta va’dalariga ishonib qolish va hokazo.
 								<br/><br/>
@@ -33,11 +34,11 @@ const AboutMe = () => {
 								<br/><br/>
 								Aksariyat odamlarni bu muammolarga uchrashini sababi…
 								<br/><br/>
-								Bu - ularni ko’chmas mulk bo’yicha yetarlicha bilimga ega emasliklaridir.
+								Bu - ularni ko’chmas mulk bo’yicha <span className={'bg-[#faee2b]'}> yetarlicha bilimga ega emasliklaridir.</span>
 								<br/><br/>
 								Bilimlari yo’qligi uchun esa…
 								<br/><br/>
-								“Endi mani ham uyim bo’ladi” deb yaxshi niyatlarda yeg’gan
+								<b>“Endi mani ham uyim bo’ladi” </b>deb yaxshi niyatlarda yeg’gan
 								<br/><br/>
 								$60,000…
 								<br/><br/>
@@ -48,14 +49,6 @@ const AboutMe = () => {
 								Bunday holatlarni juda ko’p kuzatdim.
 								<br/><br/>
 								Bilasizmi, ko’pchilik uy sotib olishdan oldin asosan nimaga ko’proq e’tibor berishadi?
-								<br/><br/>
-								Narxiga!
-								<br/><br/>
-								Naxi qulay bo’lsa tezda dogovor qilib yuborishadi!
-								<br/><br/>
-								
-								Uyni tarixiga…
-							
 							</p>
 						</div>
 						
@@ -67,27 +60,21 @@ const AboutMe = () => {
 					
 					<div className={'px-[200px] max-lg:px-[50px]'}>
 						<p className={'text-lg'}>
+							<b>NARXIGA!</b>
+							<br/><br/>
+							Naxi qulay bo’lsa tezda dogovor qilib yuborishadi!
+							<br/><br/>
+							Uyni tarixiga…
+							<br/><br/>
 							Kerakli xujjatlariga…
 							<br/><br/>
 							Shartnoma ko’proq quruvchini foydasigami yoki mijozni foydasiga tuzilganmi?
 							<br/><br/>
 							Bu narsalarga e’tibor berishmaydi.
 							<br/><br/>
-							Quruvchini ishonchliligi bilan xujjatlari joyida bo’lmasa, uyingizni qurilishi ancha cho’zilib ketishi mumkin
+							Quruvchini ishonchliligi bilan xujjatlari joyida bo’lmasa…
 							<br/><br/>
-							Yoki quruvchi pullaringizni o’zlashtirib, ertaga “bankrot bo’ldik” deb e’lon qilib yuborishi ham hech gap emas.
-							<br/><br/>
-							Undan tashqari, agar hujjatlari joyida bo’lmasa bino qurilib bo’lganidan keyin kadastri chiqmasligi mumkin.
-							<br/><br/>
-							Qurilish sifatsiz bo’lib qolishi mumkin…
-							<br/><br/>
-							Bularning barchasi uy sotib olish bo’yicha yetarlicha bilimga ega bo’lmaganlik sabab yuz beradi.
-							<br/><br/>
-							Agar siz ham yaqin orada uy olishni reja qilib yurgan bo’lsangiz…
-							<br/><br/>
-							Xavotirga o’rin yo’q.
-							<br/><br/>
-							Biz jamoamiz bilan sizga ko’chmas mulk bo’yicha bilimingizni oshirishga yordam beradigan  video darsliklar ishlab chiqdik.
+							<span className={'bg-[#faee2b]'}> Uyingizni qurilishi ancha cho’zilib ketishi mumkin.</span>
 						</p>
 					</div>
 				
--- app/(root)/_components/Banner.jsx
+++ app/(root)/_components/Banner.jsx
@@ -1,14 +1,38 @@
+import { Button } from '@/components/ui/button'
+import { Check } from 'lucide-react';
+
+
 const Banner = () => {
 	return (
-		<div className={'bg-black w-full py-16'}>
+		<div className={'bg-[#db4034] w-full py-16'}>
 			
 			
 			<div>
-				<div className={'w-full text-center text-white mb-[17rem]'}>
-					<h1 className={' text-5xl oswald font-[74px] text-center leading-normal pb-5'}>UY SOTIB OLISHDA  <span className={'text-[#feebbd]'}> ALDANIB QOLMANG!</span></h1>
-					<p className={'border-[1px] rounded-xl p-5 border-[#feebbd] w-[45%] max-lg:w-[80%] mx-auto text-center text-lg max-xl:justify-center px-5'}>2024-yilda uy sotib olishdan oldin nimalarga e’tibor berishingiz kerak?</p>
+				<div className={'w-full text-center text-white p-3'}>
+					<h1 className={'oswald text-8xl font-semibold text-center leading-normal pb-5'}>UY SOTIB OLISHDA <br/> ALDANIB QOLMANG!</h1>
+					<p className={'mx-auto text-center text-2xl font-semibold'}>2024-yilda uy sotib olishdan oldin nimalarga e’tibor berishingiz kerak?</p>
+					<Button className={'mt-24 bg-gradient-to-tr from-gray-800 to-gray-700 hover:bg-black w-[50%] py-16 xl:text-4xl lg:text-3xl md:text-2xl max-md:text-xl max-sm:text-lg uppercase'}>Hoziroq Darslikni olish</Button>
+					<p className={'text-center text-2xl mt-7'}>Shoshiling! Joylar soni kam qoldi! </p>
+					<div className={'w-full flex justify-center space-x-1 mt-3'}>
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[15px] h-[22px] bg-[#583c3b]'} />
+						<div className={'w-[17px] h-[22px] bg-[#a2bc1e]'}><Check className={'w-4 pt-[1px] h-5'}/></div>
+					
+					</div>
 				</div>
-				
+			
 			</div>
 		
 		</div>
