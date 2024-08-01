9e7834fd4293956bcc98dabf4af89abbe9e4ed66
ask250 <aslrahidov250@gmail.com>
2024-08-01 14:34:55 +0500

    update

--- app/(root)/_components/Days.jsx
+++ app/(root)/_components/Days.jsx
@@ -3,21 +3,22 @@ import { FaTelegramPlane } from 'react-icons/fa'
 
 const Days = () => {
 	return (
-		<div className={'px-[10%] bg-black w-full h-full pb-10'}>
-			<h1 className={'py-24 text-center text-4xl  max-md:text-3xl'}>
-				<span className={'inter text-white font-semibold'}>Bugunoq Video darsliklarni olib, quyidagi <span className={'text-[#feebbd]'}> bonuslarga ham Bepul</span> ega bo’lasiz</span>
+		<div className={'px-[10%] text-black w-full h-full pb-10'}>
+			<h1 className={'py-24 text-center text-8xl  max-md:text-4xl'}>
+				<span className={'oswald  font-semibold'}>Bugunoq Video darsliklarni olib, quyidagi bonuslarga ham Bepul ega bo’lasiz</span>
 			</h1>
-			<div className={'grid grid-cols-2 max-md:grid-cols-1 gap-10 mb-0'}>
+			<div className={'space-y-[150px]'}>
 				
 				<div>
-					<div className={'border rounded-xl text-white border-[#feebbd] p-5 flex'}>
-						<div className={'text-lg space-y-10'}>
-							<p className={'text-2xl text-[#feebbd] text-center'}>Yopiq Telergam kanalga qo’shilasiz</p>
-							<FaTelegramPlane className={'w-[10rem] mx-auto h-[10rem] mb-[10%]'}/>
-							<p> Ushbu kanalga:</p>
-							<p>• Qurilishga oid yangiliklar </p>
-							<p>• Maslahatlar </p>
-							<p>• Va endi katlavan paytidagi ishonchli qurilish <br /> kompaniyalar ro’yxatini tashlab
+					<div className={''}>
+						<div className={'text-2xl space-y-10'}>
+							<p className={'text-2xl bg-[#db4034] text-white w-[200px] rounded-md mx-auto text-center italic'}>BONUS #1</p>
+							<p className={'text-6xl max-md:text-5xl font-[1000] text-center'}>Yopiq Telegram kanalga qo’shilasiz</p>
+							<FaTelegramPlane className={'w-[10rem] mx-auto h-[10rem] my-5'} />
+							<p className={'w-full text-center'}> Ushbu kanalga:</p>
+							<p className={'w-full text-center'}>• Qurilishga oid yangiliklar </p>
+							<p className={'w-full text-center'}>• Maslahatlar </p>
+							<p className={'w-full text-center'}>• Va endi katlavan paytidagi ishonchli qurilish <br /> kompaniyalar ro’yxatini tashlab
 								boramiz.</p>
 						</div>
 					</div>
@@ -25,77 +26,100 @@ const Days = () => {
 				
 				<div>
 					
-					<div className={'border rounded-xl text-white border-[#feebbd] p-5 h-full'}>
+					<div className={''}>
 						
-						<div className={'text-lg block'}>
-							<p className={'text-2xl text-[#feebbd] pb-5 text-center'}> 24/7 Savollaringizga Javob olasiz</p>
-							<Headset className={'w-[10rem] mx-auto h-[10rem] mb-[10%]'} />
+						<div className={'text-2xl space-y-10'}>
+							<p className={'text-2xl bg-[#db4034] text-white w-[200px] rounded-md mx-auto text-center italic'}>BONUS #2</p>
 							
-							<p className={'text-center'}>Sizni istalgan savollaringizga javob berib turadigan support xizmatimiz bo’ladi.</p>
+							<p className={'text-6xl max-md:text-4xl font-[1000] text-center'}> 24/7 Savollaringizga Javob olasiz</p>
+							<Headset className={'w-[10rem] mx-auto h-[10rem] my-5'} />
+							
+							<p className={'text-center'}>Sizni istalgan savollaringizga javob berib turadigan support
+								xizmatimiz bo’ladi.</p>
 						</div>
 					
 					</div>
 				</div>
 				<div>
-					<div className={'border rounded-xl text-white border-[#feebbd] p-5 flex h-full'}>
-						<div className={'text-lg space-y-7 pt-5'}>
-							<p className={'text-2xl text-[#feebbd] text-center'}>Bepul Konsultatsiyalarda qatnashasiz</p>
-							<Video className={'w-[10rem] mx-auto h-[10rem] mb-[10%]'}/>
-							<p> Har oyda bir marta yoki ikki marta onlayn konsultatsiyalarni yo’lga qo’yamiz</p>
-							<p> U yerda siz o’z ishini mutaxassilaridan va yuristlardan savollaringizga javob
+					<div className={''}>
+						<div className={'text-2xl space-y-10'}>
+							<p className={'text-2xl bg-[#db4034] text-white w-[200px] rounded-md mx-auto text-center italic'}>BONUS #3</p>
+							
+							<p className={'text-6xl max-md:text-4xl font-[1000] text-center'}>Bepul Konsultatsiyalarda qatnashasiz</p>
+							<Video className={'w-[10rem] mx-auto h-[10rem] my-5'} />
+							<p className={'w-full text-center'}> Har oyda bir marta
+								yoki ikki marta onlayn konsultatsiyalarni yo’lga qo’yamiz</p>
+							<p className={'w-full text-center'}> U yerda siz o’z ishini
+								mutaxassilaridan va yuristlardan savollaringizga javob
 								olishingiz mumkin bo’ladi.</p>
 						</div>
 					</div>
 				</div>
 				
 				<div>
-					<div className={'border rounded-xl text-white border-[#feebbd] p-5 flex h-full'}>
-						<div className={'text-lg space-y-7'}>
-							<p className={'text-2xl text-[#feebbd] text-center'}>Uy sotib olishda chegirmaga ega bo’lasiz</p>
-							<BadgeDollarSign className={'w-[10rem] mx-auto h-[10rem] mb-[10%]'} />
-							<p> Yopiq telegram kanalimizda ishonchli qurilish kompaniyalar ro’yxatini berib
+					<div className={''}>
+						<div className={'text-2xl space-y-10'}>
+							<p className={'text-2xl bg-[#db4034] text-white w-[200px] rounded-md mx-auto text-center italic'}>BONUS
+								#4</p>
+							
+							<p className={'text-6xl max-md:text-4xl font-[1000] text-center'}>Uy sotib olishda chegirmaga ega bo’lasiz</p>
+							<BadgeDollarSign className={'w-[10rem] mx-auto h-[10rem] my-5'} />
+							<p className={'w-full text-center'}> Yopiq telegram kanalimizda ishonchli qurilish
+								kompaniyalar ro’yxatini berib
 								boramiz.</p>
-							<p> Agar siz bizni nomimiz orqali ushbu kompaniyalardan uy xarid qiladigan bo’lsangiz, </p>
-							<p> 1% chegirmani ham qo’lga kiritasiz. </p>
+							<p className={'w-full text-center'}> Agar siz bizni nomimiz orqali ushbu kompaniyalardan uy
+								xarid qiladigan bo’lsangiz, </p>
+							<p className={'w-full text-center'}> 1% chegirmani ham qo’lga kiritasiz. </p>
 						</div>
 					</div>
 				</div>
 				
 				<div>
-					<div className={'border rounded-xl text-white border-[#feebbd] p-5 flex h-full'}>
-						<div className={'text-lg space-y-7'}>
-							<p className={'text-2xl text-[#feebbd] text-center'}>Bonus darslarga ega bo’lasiz</p>
-							<BookOpenCheck className={'w-[10rem] mx-auto h-[10rem] mb-[10%]'}/>
-							<p> Har oyda bir marta qurilishga oid kuchli spikerlarni chaqiramiz, ma’lumotlar
+					<div className={''}>
+						<div className={'text-2xl space-y-10'}>
+							<p className={'text-2xl bg-[#db4034] text-white w-[200px] rounded-md mx-auto text-center italic'}>BONUS
+								#5</p>
+							
+							<p className={'text-6xl max-md:text-4xl font-[1000] text-center'}>Bonus darslarga ega bo’lasiz</p>
+							<BookOpenCheck className={'w-[10rem] mx-auto h-[10rem] my-5'} />
+							<p className={'w-full text-center'}> Har oyda bir marta qurilishga oid kuchli spikerlarni
+								chaqiramiz, ma’lumotlar
 								olamiz </p>
-							<p> Va ushbu ma’lumotlarni yopiq telegram kanalga yuklab boramiz. </p>
-							<p> Spikerlardan qanday qilib quruvchilarga aldanib qolmaslik, qanday qilib uylarni to’g’ri
+							<p className={'w-full text-center'}> Va ushbu ma’lumotlarni yopiq telegram kanalga yuklab
+								boramiz. </p>
+							<p className={'w-full text-center'}> Spikerlardan qanday qilib quruvchilarga aldanib
+								qolmaslik, qanday qilib uylarni to’g’ri
 								xarid qilish va tezroq sotish bo’yicha qo’shimcha bilimlarga ham ega bo’lasiz.</p>
 						</div>
 					</div>
 				</div>
 				
 				<div>
-					<div className={'border rounded-xl text-white border-[#feebbd] p-5 flex'}>
-						<div className={'text-lg space-y-3'}>
-							<p className={'text-2xl text-[#feebbd] text-center  '}>Qiziqarli o’yinlarda ishtirok
+					<div className={''}>
+						<div className={'text-2xl space-y-10    '}>
+							<p className={'text-2xl bg-[#db4034] text-white w-[200px] rounded-md mx-auto text-center italic'}>BONUS
+								#6</p>
+							<p className={'text-6xl max-md:text-4xl font-[1000] text-center'}>Qiziqarli o’yinlarda ishtirok
 								etasiz</p>
-							<Gamepad2 className={'w-[10rem] mx-auto h-[10rem] mb-[10%]'} />
-							<p> Darslarimizni ko’rganingizdan so’ng, bemalol ishonchli uylar ro’yxatini tuzib olib,
+							<Gamepad2 className={'w-[10rem] mx-auto h-[10rem] my-5'} />
+							<p className={'w-full text-center'}> Darslarimizni ko’rganingizdan so’ng, bemalol ishonchli
+								uylar ro’yxatini tuzib olib,
 								o’zingiz xarid qilishingiz mumkin.</p>
 							<br />
-							<p> Yokida buni bizga ham topshirishingiz mumkin. </p>
+							<p className={'w-full text-center'}> Yokida buni bizga ham topshirishingiz mumkin. </p>
 							<br />
-							<p> Biz sizga o’zimiz hammasini ko’rib beramiz.</p>
+							<p className={'w-full text-center'}> Biz sizga o’zimiz hammasini ko’rib beramiz.</p>
 							<br />
-							<p> Bu xizmatimiz uchun qo’shimcha to’lovlar mavjud.</p>
+							<p className={'w-full text-center'}> Bu xizmatimiz uchun qo’shimcha to’lovlar mavjud.</p>
 							<br />
-							<p> Darslarni xarid qilib, telegram guruhga qo’shiladigan bo’lsangiz, biz har oy 5ta
+							<p className={'w-full text-center'}> Darslarni xarid qilib, telegram guruhga qo’shiladigan
+								bo’lsangiz, biz har oy 5ta
 								ixtiyor kishini tanlab olamiz </p>
 							<br />
-							<p> Va ularga uyni ko’rib berish xizmatimizni bepulga taqdim etamiz. </p>
+							<p className={'w-full text-center'}> Va ularga uyni ko’rib berish xizmatimizni bepulga
+								taqdim etamiz. </p>
 							<br />
-							<p> Bular orasida albatta siz ham bo’lishingiz mumkin.</p>
+							<p className={'w-full text-center'}> Bular orasida albatta siz ham bo’lishingiz mumkin.</p>
 						</div>
 					</div>
 				
--- app/(root)/_components/Opportunity.jsx
+++ app/(root)/_components/Opportunity.jsx
@@ -2,60 +2,61 @@ import Image from 'next/image'
 
 const Opportunity = () => {
 	return(
-		<div className={'bg-black text-white'}>
-			<div className={'md:flex justify-between leading-relaxed md:mx-[10%] py-10'}>
-				<h1 className={' text-5xl max-w-3xl max-md:text-3xl leading-[1.5] font-semibold max-lg:w-full max-lg:text-center'}>
-					<span className={'text-[#feebbd]'}>Video Darslik Sizga Qanday Foyda Beradi?</span></h1>
+		<div className={' text-white py-10'}>
+			<div className={'py-10 w-full text-center'}>
+				<h1 className={'text-6xl max-md:text-4xl  leading-[1.5] font-semibold max-lg:w-full max-lg:text-center'}>
+					<span className={'text-black oswald'}>Video Darslik Sizga Qanday Foyda Beradi?</span></h1>
 			</div>
 			
-			<div className={'mx-[10%] mt-10 grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-10'}>
-				<div className={'bg-black rounded-lg p-5 border border-[#feebbd]'}>
-					<h1 className={'text-2xl'}>Modul 1</h1>
-					<hr className={'w-full p-px'} />
+			<div className={' mt-10 w-[50%] mx-auto max-md:w-[70%] text-black'}>
+				<div className={'p-5'}>
+					<hr className={'w-full'} />
 					<div className={'space-y-3 '}>
-						<p className={'mt-3 font-semibold text-2xl mb-5 text-[#feebbd]'}> Qanaqa qilib uylarni to’g’ri sotib olishni o’rganasiz</p>
-						<p className={'max-w-xs text-lg pt-4'}>- Qaysi etajdan uy tanlash kerak?</p>
-						<p className={' max-w-xs text-lg pt-4'}>- Qaysi lokatsiyadan uy olish kerak?</p>
-						<p className={' max-w-xs text-lg pt-4'}>- Quruvchini ishonchliligini qanaqa qilib tekshirsa bo’ladi? </p>
-						<p className={' max-w-xs text-lg pt-4'}>- Qanaqa qilib quruvchi haqida to’liq ma’lumot olsa bo’ladi? </p>
-						<p className={' max-w-xs text-lg pt-4'}>Shuni hammasini bilib olasiz.</p>
+						<p className={'mt-3 font-semibold text-3xl mb-5'}><span className={'text-[#db4034] text-3xl pr-2'}>•</span>Qanday qilib uylarni to’g’ri sotib olishni o’rganasiz</p>
+						<p className={' text-xl pt-4'}>- Qaysi etajdan uy tanlash kerak?</p>
+						<p className={'  text-xl pt-4'}>- Qaysi lokatsiyadan uy olish kerak?</p>
+						<p className={' text-xl pt-4'}>- Quruvchini ishonchliligini qanaqa qilib tekshirsa bo’ladi? </p>
+						<p className={'  text-xl pt-4'}>- Qanaqa qilib quruvchi haqida to’liq ma’lumot olsa bo’ladi? </p>
+						<p className={' text-xl pt-4'}>Shuni hammasini bilib olasiz.</p>
 					</div>
 				
 				
 				</div>
 				
-				<div className={'bg-black rounded-lg p-5 border border-[#feebbd]'}>
-					<h1 className={'text-2xl'}>Modul 2</h1>
+				<div className={''}>
 					<hr className={'w-full p-px'} />
-					<div className={'space-y-7'}>
-						<p className={'mt-3 font-semibold text-2xl mb-5 text-[#feebbd]'}>Xujjatlar bilan ishlashni
+					<div className={'space-y-7 text-xl'}>
+						<p className={'mt-3 font-semibold text-3xl mb-5'}><span
+							className={'text-[#db4034] text-3xl pr-2'}>•</span>Xujjatlar
+							bilan ishlashni
 							o’rganasiz</p>
-						<p className={' max-w-xs text-lg'}>- Quruvchini proyektga oid qanaqa xujjatlari bor yoki
-							qanaqalari yo’q?</p>
-						<p className={' max-w-xs text-lg'}>Hammasini to’liq ko’rib chiqa olasiz va to’liq tushuna
-							olasiz. </p>
-						<p className={' max-w-xs text-lg'}>Xujjatlarida qandaydir kamchiliklar bo’lsa, darhol
-							sezasiz.</p>
-						<p className={' max-w-xs text-lg'}>Undan tashqari, qurilishni qaysi etapi to’g’ri qurilyapti,
-							qayeri noto’g’ri ketyapti, hammasi reja bo’yicha ketyaptimi yoki rejaga o’zgartirish
-							kiritildimi hammasini bila olasiz.</p>
+						<p className={' text-xl'}>Quruvchini loyihaga oid qanaqa xujjatlari bor yoki qanaqalari
+							yo’q?</p>
+						<p className={'text-xl'}>Hammasini to’liq ko’rib chiqa olasiz va to’liq tushuna olasiz. </p>
+						<p className={'text-xl'}>Xujjatlarida qandaydir kamchiliklar bo’lsa, darhol sezasiz.</p>
+						<p className={'text-xl'}>Undan tashqari…</p>
+						<p className={'text-xl'}>- Qurilishni qaysi etapi to’g’ri qurilyapti?</p>
+						<p>- Qayeri noto’g’ri ketyapti?</p>
+						<p>- Hammasi reja bo’yicha ketyaptimi yoki rejaga o’zgartirish kiritildimi…</p>
+						<p>Hammasini bila olasiz.</p>
 					</div>
 				
 				
 				</div>
 				
-				<div className={'bg-black rounded-lg p-5 border border-[#feebbd]'}>
-					<h1 className={'text-2xl'}>Modul 3</h1>
-					<hr className={'w-full p-px'} />
-					<div className={'space-y-7'}>
-						<p className={'mt-3 font-semibold text-2xl mb-5 text-[#feebbd]'}>Shartnoma bilan ishlashni
+				<div className={''}>
+				<hr className={'w-full p-px'} />
+					<div className={'space-y-7 text-xl'}>
+						<p className={'mt-3 font-semibold text-3xl mb-5 '}><span className={'text-[#db4034] text-3xl pr-2'}>•</span>Shartnoma
+							bilan ishlashni
 							o’rganasiz.</p>
-						<p className={' max-w-xs text-lg'}>- Shartnomada qaysi bandlar bo’lishi kerak? </p>
-						<p className={' max-w-xs text-lg'}>- Shartoma tuzayotganda nimalarga e’tibor berish kerak? </p>
-						<p className={'max-w-xs text-lg'}>- Quruvchi bilan ertaga tushunmovchiliklar kelib chiqadigan
-							bo’lsa, siz yutib chiqishingiz uchun shartnomaga qanday bandlarni qo’shtirib olishingiz
-							kerak? </p>
-						<p className={' max-w-xs text-lg'}>Shu bo’yicha to’liq ma’lumotlarga ega bo’lasiz.</p>
+						<p className={' text-xl'}>- Shartnomada qaysi bandlar bo’lishi kerak?  </p>
+						<p className={' text-xl'}>- Shartoma tuzayotganda nimalarga e’tibor berish kerak? </p>
+						<p className={' text-xl'}>- Quruvchi bilan ertaga tushunmovchiliklar kelib chiqadigan bo’lsa…</p>
+						<p className={' max-w-xs text-xl'}>Siz yutib chiqishingiz uchun shartnomaga qanday bandlarni qo’shtirib olishingiz kerak? </p>
+						<p>Shu bo’yicha to’liq ma’lumotlarga ega bo’lasiz.</p>
+						<p>Bu hali hammasi emas…</p>
+						<p>Bugunoq video darsliklarni olib, quyidagi bepul bonuslarga ham ega bo’lasiz!</p>
 					</div>
 				
 				
